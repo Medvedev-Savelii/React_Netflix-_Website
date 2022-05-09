5dfe4e3603e426151af2ab984111cb0552b09d26
sava <savamedvedev@gmail.com>
2022-05-09 21:53:52 +0800

    first-commit

---
+++ src/components/listItem/ListItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './ListItem.scss';
+const ListItem = () => {
+  return (
+	<div>ListItem</div>
+  )
+}
+
+export default ListItem
---
+++ src/pages/watch/Watch.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './watch.scss';
+const Watch = () => {
+  return (
+	<div>Watch</div>
+  )
+}
+
+export default Watch
